11569764ba74150ee8b51bfbd9311d215f301140
Jamsheedbek <jamshidbekml@gmail.com>
2022-10-26 18:52:14 +0500

    fixed connection string sequelize

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,13 @@ require("dotenv").config();
 const connectionString = process.env.CONNECTION_STRING;
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(connectionString);
+const sequelize = new Sequelize(
+  "postgres://postgres:pgpwd@localhost:5432/gidro",
+  {
+    host: "localhost",
+    dialect: "postgres",
+  }
+);
 
 const Users = sequelize.define("users", {
   id: {
