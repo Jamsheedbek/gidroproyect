4121ed9bcea6126acd907e747e41977bfcb94588
jamshidbekml <jamshidbekml@gmail.com>
2022-12-07 15:02:55 +0500

    edited view adim

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -3,133 +3,141 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = {
-  createProject: async (req, res) => {
-    try {
-      const { name, content, type } = req.body;
-      const file = req.files.file;
-      const fileName = file.name;
+    createProject: async (req, res) => {
+        try {
+            const { name, content, type } = req.body;
+            const file = req.files.file;
+            const fileName = file.name;
 
-      const newProject = await Projects.create({
-        name,
-        content,
-        fileName,
-        type,
-      });
-
-      const uploadPath = path.resolve(
-        "./src/uploads/assets/projects",
-        fileName
-      );
-
-      fs.writeFile(uploadPath, file.data, (err) => {
-        if (err) {
-          return res.redirect("/direksiya/admin/create/project");
-        }
-      });
-
-      res.redirect("/direksiya/admin/create/project");
-    } catch (err) {
-      console.log(err);
-      res.redirect("/direksiya/admin/create/project");
-    }
-  },
-  deleteProject: async (req, res) => {
-    try {
-      const { id, fileName } = req.body;
-
-      const Path = path.resolve("./src/uploads/assets/projects", fileName);
-
-      const files = await Projects.findAll({ where: { fileName: fileName } });
-
-      if (files.length == 1) {
-        fs.readFile(Path, async (err, data) => {
-          if (err) {
-            console.log(err.message);
-          }
-          if (data) {
-            fs.unlink(Path, function (err) {
-              if (err) return res.redirect("/direksiya/admin/get/projects");
+            const newProject = await Projects.create({
+                name,
+                content,
+                fileName,
+                type,
             });
-          }
-        });
-      }
 
-      await Projects.destroy({ where: { id } });
+            const uploadPath = path.resolve(
+                "./src/uploads/assets/projects",
+                fileName
+            );
 
-      res.redirect("direksiya/admin/get/projects");
-    } catch (err) {
-      console.log(err);
-      res.redirect("/direksiya/admin/get/projects");
-    }
-  },
-  editProject: async (req, res) => {
-    try {
-      const { id, name, content, type } = req.body;
+            fs.writeFile(uploadPath, file.data, (err) => {
+                if (err) {
+                    return res.redirect("/direksiya/admin/create/project");
+                }
+            });
 
-      const image = req.files;
-
-      const oldProject = await Projects.findOne({ where: { id: id } });
-
-      if (image) {
-        const fileName = image.file.name;
-        const files = await Projects.findAll({
-          where: { fileName: oldProject.dataValues.fileName },
-        });
-
-        if (files.length == 1) {
-          fs.unlink(
-            path.resolve(
-              "./src/uploads/assets/projects",
-              oldProject.dataValues.fileName
-            ),
-            function (err) {
-              if (err) return console.log(err);
-            }
-          );
+            res.redirect("/direksiya/admin/create/project");
+        } catch (err) {
+            console.log(err);
+            res.redirect("/direksiya/admin/create/project");
         }
+    },
+    deleteProject: async (req, res) => {
+        try {
+            const { id, fileName } = req.body;
 
-        let project = await Projects.update(
-          {
-            name,
-            content,
-            fileName,
-            type: type,
-          },
-          {
-            where: {
-              id: id,
-            },
-          }
-        );
+            const Path = path.resolve(
+                "./src/uploads/assets/projects",
+                fileName
+            );
 
-        const uploadPath = path.resolve(
-          "./src/uploads/assets/projects",
-          fileName
-        );
+            const files = await Projects.findAll({
+                where: { fileName: fileName },
+            });
 
-        fs.writeFile(uploadPath, image.file.data, (err) => {
-          console.log(err);
-        });
-      } else {
-        let project = await Projects.update(
-          {
-            name,
-            content,
-            fileName: oldProject.dataValues.fileName,
-            type: type,
-          },
-          {
-            where: {
-              id: id,
-            },
-          }
-        );
-      }
+            if (files.length == 1) {
+                fs.readFile(Path, async (err, data) => {
+                    if (err) {
+                        console.log(err.message);
+                    }
+                    if (data) {
+                        fs.unlink(Path, function (err) {
+                            if (err)
+                                return res.redirect(
+                                    "/direksiya/admin/get/projects"
+                                );
+                        });
+                    }
+                });
+            }
 
-      res.redirect("/direksiya/admin/get/projects");
-    } catch (err) {
-      console.log(err);
-      res.redirect("/direksiya/admin/get/projects");
-    }
-  },
+            await Projects.destroy({ where: { id: Number(id) } });
+
+            res.redirect("direksiya/admin/get/projects");
+        } catch (err) {
+            console.log(err);
+            res.redirect("/direksiya/admin/get/projects");
+        }
+    },
+    editProject: async (req, res) => {
+        try {
+            const { id, name, content, type } = req.body;
+
+            const image = req.files;
+
+            const oldProject = await Projects.findOne({ where: { id: id } });
+
+            if (image) {
+                const fileName = image.file.name;
+                const files = await Projects.findAll({
+                    where: { fileName: oldProject.dataValues.fileName },
+                });
+
+                if (files.length == 1) {
+                    fs.unlink(
+                        path.resolve(
+                            "./src/uploads/assets/projects",
+                            oldProject.dataValues.fileName
+                        ),
+                        function (err) {
+                            if (err) return console.log(err);
+                        }
+                    );
+                }
+
+                let project = await Projects.update(
+                    {
+                        name,
+                        content,
+                        fileName,
+                        type: type,
+                    },
+                    {
+                        where: {
+                            id: Number(id),
+                        },
+                    }
+                );
+
+                const uploadPath = path.resolve(
+                    "./src/uploads/assets/projects",
+                    fileName
+                );
+
+                fs.writeFile(uploadPath, image.file.data, (err) => {
+                    console.log(err);
+                });
+            } else {
+                let project = await Projects.update(
+                    {
+                        name,
+                        content,
+                        fileName: oldProject.dataValues.fileName,
+                        type: type,
+                    },
+                    {
+                        where: {
+                            id: Number(id),
+                        },
+                    }
+                );
+            }
+
+            res.redirect("/direksiya/admin/get/projects");
+        } catch (err) {
+            console.log(err);
+            res.redirect("/direksiya/admin/get/projects");
+        }
+    },
 };
