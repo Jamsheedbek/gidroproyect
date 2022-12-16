37f7e93be70520b5abd28f2dd60134999e147c04
jamshidbekml <jamshidbekml@gmail.com>
2022-12-16 19:59:32 +0500

    added leagal status page

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,95 +14,112 @@ const workController = require("../controllers/work.controller");
 const verifyToken = require("../middleware/authJwt");
 
 let routes = (app) => {
-  router
-    //pages
-    .get("/", pageController.homePage)
-    .get("/projects/current", pageController.finishedProjectsPage)
-    .get("/projects/promising", pageController.plannedProjectsPage)
-    .get("/direksiya/login", pageController.loginPage)
+    router
+        //pages
+        .get("/", pageController.homePage)
+        .get("/projects/current", pageController.finishedProjectsPage)
+        .get("/projects/promising", pageController.plannedProjectsPage)
+        .get("/direksiya/login", pageController.loginPage)
 
-    // News page
-    .get(
-      "/direksiya/admin/create/news",
-      verifyToken,
-      pageController.createNewsPage
-    )
-    .get("/direksiya/admin/get/news", verifyToken, pageController.getNewsPage)
-    .get(
-      "/direksiya/admin/edit/news/:id",
-      verifyToken,
-      pageController.editNewsPage
-    )
-    .get("/news/view/:id", pageController.newsPage)
-    .get("/news/company", pageController.companyNewsPage)
-    .get("/news/world", pageController.worldNewsPage)
-    .get("/tenders", pageController.tendersPage)
+        // News page
+        .get(
+            "/direksiya/admin/create/news",
+            verifyToken,
+            pageController.createNewsPage
+        )
+        .get(
+            "/direksiya/admin/get/news",
+            verifyToken,
+            pageController.getNewsPage
+        )
+        .get(
+            "/direksiya/admin/edit/news/:id",
+            verifyToken,
+            pageController.editNewsPage
+        )
+        .get("/news/view/:id", pageController.newsPage)
+        .get("/news/company", pageController.companyNewsPage)
+        .get("/news/world", pageController.worldNewsPage)
+        .get("/tenders", pageController.tendersPage)
+        .get("/status", pageController.legalStatus)
 
-    // Projects page
-    .get(
-      "/direksiya/admin/create/project",
-      verifyToken,
-      pageController.createProjectPage
-    )
-    .get(
-      "/direksiya/admin/get/projects",
-      verifyToken,
-      pageController.getProjectsPage
-    )
-    .get(
-      "/direksiya/admin/edit/project/:id",
-      verifyToken,
-      pageController.editProjectPage
-    )
-    .get("/projects/view/:id", pageController.projectPage)
+        // Projects page
+        .get(
+            "/direksiya/admin/create/project",
+            verifyToken,
+            pageController.createProjectPage
+        )
+        .get(
+            "/direksiya/admin/get/projects",
+            verifyToken,
+            pageController.getProjectsPage
+        )
+        .get(
+            "/direksiya/admin/edit/project/:id",
+            verifyToken,
+            pageController.editProjectPage
+        )
+        .get("/projects/view/:id", pageController.projectPage)
 
-    // Users page
-    .get("/direksiya/admin/get/users", verifyToken, pageController.usersPage)
-    .get("/direksiya/users", verifyToken, pageController.userPage)
+        // Users page
+        .get(
+            "/direksiya/admin/get/users",
+            verifyToken,
+            pageController.usersPage
+        )
+        .get("/direksiya/users", verifyToken, pageController.userPage)
 
-    // Works page
-    .get("/direksiya/admin/get/works", verifyToken, pageController.worksPage)
+        // Works page
+        .get(
+            "/direksiya/admin/get/works",
+            verifyToken,
+            pageController.worksPage
+        )
 
-    // Liders page
-    .get(
-      "/direksiya/admin/create/liders",
-      verifyToken,
-      pageController.createLiders
-    )
-    .get("/lidership", pageController.leadershipPage)
+        // Liders page
+        .get(
+            "/direksiya/admin/create/liders",
+            verifyToken,
+            pageController.createLiders
+        )
+        .get("/lidership", pageController.leadershipPage)
 
-    // Images page
-    .get("/direksiya/admin/get/images", verifyToken, pageController.editImages)
+        // Images page
+        .get(
+            "/direksiya/admin/get/images",
+            verifyToken,
+            pageController.editImages
+        )
 
-    // news controller
-    .post("/news", newsController.createNews)
-    .post("/delete-news", newsController.deleteNews)
-    .post("/edit-news", newsController.editNews)
+        // news controller
+        .post("/news", newsController.createNews)
+        .post("/delete-news", newsController.deleteNews)
+        .post("/edit-news", newsController.editNews)
 
-    //projects controller
-    .post("/projects", projectsController.createProject)
-    .post("/delete-project", projectsController.deleteProject)
-    .post("/edit-project", projectsController.editProject)
+        //projects controller
+        .post("/projects", projectsController.createProject)
+        .post("/delete-project", projectsController.deleteProject)
+        .post("/edit-project", projectsController.editProject)
 
-    //login controller
-    .post("/direksiya/login", authController.signIn)
-    .post("/log-out", usersController.logOutUser)
+        //login controller
+        .post("/direksiya/login", authController.signIn)
+        .post("/log-out", usersController.logOutUser)
 
-    //users controller
-    .post("/create-user", usersController.createUser)
-    .post("/edit-user", usersController.editUser)
-    .post("/delete-user", usersController.deleteUser)
+        //users controller
+        .post("/create-user", usersController.createUser)
+        .post("/edit-user", usersController.editUser)
+        .post("/delete-user", usersController.deleteUser)
 
-    // works controller
-    .post("/create-work", workController.uploadWork)
-    .post("/delete-work", workController.deleteWork)
-    .get("/download/:name", verifyToken, workController.downloadWork)
+        // works controller
+        .post("/create-work", workController.uploadWork)
+        .post("/delete-work", workController.deleteWork)
+        .get("/download/:name", verifyToken, workController.downloadWork)
 
-    // carousel controller
-    .post("/create-carousel", carouselController.createCarousel)
-    .post("/edit-carousel", carouselController.updateCarousel);
+        // carousel controller
+        .post("/create-carousel", carouselController.createCarousel)
+        .post("/edit-carousel", carouselController.updateCarousel);
 
-  app.use(router);
+    app.use(router);
 };
 
 module.exports = routes;
