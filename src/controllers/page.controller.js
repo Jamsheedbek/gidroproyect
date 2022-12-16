74f2c7ad1780725d99ac3609193890226b5dbfe5
jamshidbekml <jamshidbekml@gmail.com>
2022-12-16 19:55:18 +0500

    added leagal status page

--- src/controllers/page.controller.js
+++ src/controllers/page.controller.js
@@ -9,129 +9,141 @@ const handleWorks = require("../util/handleWorks");
 const { verifyUser } = require("../util/jwt");
 
 module.exports = {
-  homePage: async (req, res) => {
-    try {
-      const news = await handleAllNews();
-      const projects = await handleProjects();
-      const currency = await handleCurrency();
-      const wheather = await handleWheather();
-      const carousel = await handleCarousel();
+    homePage: async (req, res) => {
+        try {
+            const news = await handleAllNews();
+            const projects = await handleProjects();
+            const currency = await handleCurrency();
+            const wheather = await handleWheather();
+            const carousel = await handleCarousel();
 
-      res.render("index", { news, projects, wheather, currency, carousel });
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  worldNewsPage: async (req, res) => {
-    try {
-      const news = await handleAllNews();
+            res.render("index", {
+                news,
+                projects,
+                wheather,
+                currency,
+                carousel,
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    worldNewsPage: async (req, res) => {
+        try {
+            const news = await handleAllNews();
 
-      res.render("worldNews", { news });
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  companyNewsPage: async (req, res) => {
-    try {
-      const news = await handleAllNews();
+            res.render("worldNews", { news });
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    companyNewsPage: async (req, res) => {
+        try {
+            const news = await handleAllNews();
 
-      res.render("companyNews", { news });
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  finishedProjectsPage: async (req, res) => {
-    const projects = await handleProjects();
-    res.render("finishedProjects", { projects });
-  },
-  plannedProjectsPage: async (req, res) => {
-    const projects = await handleProjects();
-    res.render("plannedProjects", { projects });
-  },
-  loginPage: (req, res) => {
-    res.render("login", { message: null });
-  },
-  createNewsPage: async (req, res) => {
-    res.render("admin", {
-      page: "create-news",
-    });
-  },
-  getNewsPage: async (req, res) => {
-    const news = await handleAllNews();
-    res.render("admin", {
-      news,
-      page: "get-news",
-    });
-  },
-  editNewsPage: async (req, res) => {
-    const news = await handleAllNews(req.params.id);
-    res.render("admin", { page: "edit-news", news: news.dataValues });
-  },
-  createProjectPage: async (req, res) => {
-    res.render("admin", {
-      page: "create-project",
-    });
-  },
-  getProjectsPage: async (req, res) => {
-    const projects = await handleProjects();
-    res.render("admin", {
-      projects,
-      page: "get-projects",
-    });
-  },
-  editProjectPage: async (req, res) => {
-    const project = await handleProjects(req.params.id);
+            res.render("companyNews", { news });
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    finishedProjectsPage: async (req, res) => {
+        const projects = await handleProjects();
+        res.render("finishedProjects", { projects });
+    },
+    plannedProjectsPage: async (req, res) => {
+        const projects = await handleProjects();
+        res.render("plannedProjects", { projects });
+    },
+    loginPage: (req, res) => {
+        res.render("login", { message: null });
+    },
+    createNewsPage: async (req, res) => {
+        res.render("admin", {
+            page: "create-news",
+        });
+    },
+    getNewsPage: async (req, res) => {
+        const news = await handleAllNews();
+        res.render("admin", {
+            news,
+            page: "get-news",
+        });
+    },
+    editNewsPage: async (req, res) => {
+        const news = await handleAllNews(req.params.id);
+        res.render("admin", { page: "edit-news", news: news.dataValues });
+    },
+    createProjectPage: async (req, res) => {
+        res.render("admin", {
+            page: "create-project",
+        });
+    },
+    getProjectsPage: async (req, res) => {
+        const projects = await handleProjects();
+        res.render("admin", {
+            projects,
+            page: "get-projects",
+        });
+    },
+    editProjectPage: async (req, res) => {
+        const project = await handleProjects(req.params.id);
 
-    res.render("admin", { page: "edit-project", project });
-  },
-  usersPage: async (req, res) => {
-    const users = await handleAllUsers();
-    res.render("admin", {
-      users,
-      page: "get-users",
-    });
-  },
-  worksPage: async (req, res) => {
-    const works = await handleWorks();
-    res.render("admin", {
-      works,
-      page: "get-works",
-    });
-  },
-  userPage: async (req, res) => {
-    const token = req.cookies.token;
-    const userId = verifyUser(token).id;
-    const works = await handleWorks(userId);
-    const user = await handleAllUsers(userId);
-    res.render("users", { works, user });
-  },
-  projectPage: async (req, res) => {
-    const news = await handleAllNews();
-    const activeProject = await handleProjects(req.params.id);
-    res.render("project", { activeProject, news });
-  },
-  newsPage: async (req, res) => {
-    const news = await handleAllNews();
-    const activeNews = await handleAllNews(req.params.id);
+        res.render("admin", { page: "edit-project", project });
+    },
+    usersPage: async (req, res) => {
+        const users = await handleAllUsers();
+        res.render("admin", {
+            users,
+            page: "get-users",
+        });
+    },
+    worksPage: async (req, res) => {
+        const works = await handleWorks();
+        res.render("admin", {
+            works,
+            page: "get-works",
+        });
+    },
+    userPage: async (req, res) => {
+        const token = req.cookies.token;
+        const userId = verifyUser(token).id;
+        const works = await handleWorks(userId);
+        const user = await handleAllUsers(userId);
+        res.render("users", { works, user });
+    },
+    projectPage: async (req, res) => {
+        const news = await handleAllNews();
+        const activeProject = await handleProjects(req.params.id);
+        res.render("project", { activeProject, news });
+    },
+    newsPage: async (req, res) => {
+        const news = await handleAllNews();
+        const activeNews = await handleAllNews(req.params.id);
 
-    res.render("news", { news, activeNews });
-  },
-  tendersPage: async (req, res) => {
-    const news = await handleAllNews();
+        res.render("news", { news, activeNews });
+    },
+    tendersPage: async (req, res) => {
+        const news = await handleAllNews();
 
-    res.render("tender", { news });
-  },
-  leadershipPage: async (req, res) => {
-    const news = await handleAllNews();
+        res.render("tender", { news });
+    },
+    leadershipPage: async (req, res) => {
+        const news = await handleAllNews();
 
-    res.render("lidership", { news });
-  },
-  createLiders: async (req, res) => {
-    res.render("admin", { page: "create-liders" });
-  },
-  editImages: async (req, res) => {
-    const carousel = await handleCarousel();
+        res.render("lidership", { news });
+    },
+    createLiders: async (req, res) => {
+        res.render("admin", { page: "create-liders" });
+    },
+    editImages: async (req, res) => {
+        const carousel = await handleCarousel();
 
-    res.render("admin", { page: "get-images", carousel });
-  },
+        res.render("admin", { page: "get-images", carousel });
+    },
+    legalStatus: async (req, res) => {
+        const news = await handleAllNews();
+        const activeNews = await handleAllNews({ type: "legal-status" });
+
+        res.render("aboute", { news, activeNews });
+    },
 };
--- src/util/handleNews.js
+++ src/util/handleNews.js
@@ -3,46 +3,65 @@ const moment = require("moment");
 moment.locale("uz");
 
 const handleAllNews = async (id) => {
-  try {
-    var news = [];
-    var allNews;
-    if (id) {
-      allNews = await News.findOne({ where: { news_id: id }, attributes: [
-        'news_id','title','content', 'fileName', 'type'
-      ]});
-      allNews.dataValues.imgUrl =
-        "/files/assets/news/" + allNews.dataValues.fileName;
+    try {
+        var news = [];
+        var allNews;
+        if (id.type) {
+            allNews = await News.findOne({
+                where: { type: id.type },
+                attributes: ["news_id", "title", "content", "fileName", "type"],
+            });
 
-      allNews.dataValues.date =
-        moment(allNews.dataValues.createdAt).format("L h:mm").slice(0, 11) +
-        (moment(allNews.dataValues.createdAt).format("L h:mm").slice(-5, -3) -
-          0 +
-          5) +
-        moment(allNews.dataValues.createdAt).format("L h:mm").slice(-3);
+            return allNews;
+        }
+        if (id) {
+            allNews = await News.findOne({
+                where: { news_id: id },
+                attributes: ["news_id", "title", "content", "fileName", "type"],
+            });
+            allNews.dataValues.imgUrl =
+                "/files/assets/news/" + allNews.dataValues.fileName;
 
-      return allNews;
-    } else {
-      const allNews = await News.findAll({ order: [["createdAt", "DESC"]], attributes: [
-        'news_id','title','content', 'fileName', 'type'
-      ]});
-      allNews.map((e) => {
-        e.dataValues.imgUrl = "/files/assets/news/" + e.dataValues.fileName;
+            allNews.dataValues.date =
+                moment(allNews.dataValues.createdAt)
+                    .format("L h:mm")
+                    .slice(0, 11) +
+                (moment(allNews.dataValues.createdAt)
+                    .format("L h:mm")
+                    .slice(-5, -3) -
+                    0 +
+                    5) +
+                moment(allNews.dataValues.createdAt).format("L h:mm").slice(-3);
 
-        e.dataValues.date =
-          moment(e.dataValues.createdAt).format("L h:mm").slice(0, 11) +
-          (moment(e.dataValues.createdAt).format("L h:mm").slice(-5, -3) -
-            0 +
-            5) +
-          moment(e.dataValues.createdAt).format("L h:mm").slice(-3);
+            return allNews;
+        } else {
+            const allNews = await News.findAll({
+                order: [["createdAt", "DESC"]],
+                attributes: ["news_id", "title", "content", "fileName", "type"],
+            });
+            allNews.map((e) => {
+                e.dataValues.imgUrl =
+                    "/files/assets/news/" + e.dataValues.fileName;
 
-        news.push(e.dataValues);
-      });
+                e.dataValues.date =
+                    moment(e.dataValues.createdAt)
+                        .format("L h:mm")
+                        .slice(0, 11) +
+                    (moment(e.dataValues.createdAt)
+                        .format("L h:mm")
+                        .slice(-5, -3) -
+                        0 +
+                        5) +
+                    moment(e.dataValues.createdAt).format("L h:mm").slice(-3);
+
+                news.push(e.dataValues);
+            });
+        }
+
+        return news;
+    } catch (err) {
+        console.log(err);
     }
-
-    return news;
-  } catch (err) {
-    console.log(err);
-  }
 };
 
 module.exports = handleAllNews;
