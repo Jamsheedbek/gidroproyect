feec70b56eaad3aed10f902e4f9c8cad2138baba
jamshidbekml <jamshidbekml@gmail.com>
2023-01-31 00:48:19 +0500

    add log

--- src/util/handleNews.js
+++ src/util/handleNews.js
@@ -1,16 +1,16 @@
-const { News } = require("../models");
-const moment = require("moment");
-moment.locale("uz");
+const { News } = require('../models');
+const moment = require('moment');
+moment.locale('uz');
 
 const handleAllNews = async (id) => {
     try {
         var news = [];
         var allNews;
-        if (id && id.type && id.type == "legal-status") {
+        if (id && id.type && id.type == 'legal-status') {
             console.log(`tepadagi if ishladi\n\n`);
             allNews = await News.findOne({
                 where: { type: id.type },
-                attributes: ["news_id", "title", "content", "fileName", "type"],
+                attributes: ['news_id', 'title', 'content', 'fileName', 'type'],
             });
 
             return allNews;
@@ -19,42 +19,44 @@ const handleAllNews = async (id) => {
             console.log(`pastgi if ishladi\n\n`);
             allNews = await News.findOne({
                 where: { news_id: id },
-                attributes: ["news_id", "title", "content", "fileName", "type"],
+                attributes: ['news_id', 'title', 'content', 'fileName', 'type'],
             });
             allNews.dataValues.imgUrl =
-                "/files/assets/news/" + allNews.dataValues.fileName;
+                '/files/assets/news/' + allNews.dataValues.fileName;
 
+            console.log(allNews.dataValues.date);
             allNews.dataValues.date =
                 moment(allNews.dataValues.createdAt)
-                    .format("L h:mm")
+                    .format('L h:mm')
                     .slice(0, 11) +
                 (moment(allNews.dataValues.createdAt)
-                    .format("L h:mm")
+                    .format('L h:mm')
                     .slice(-5, -3) -
                     0 +
                     5) +
-                moment(allNews.dataValues.createdAt).format("L h:mm").slice(-3);
+                moment(allNews.dataValues.createdAt).format('L h:mm').slice(-3);
 
             return allNews;
         } else {
             const allNews = await News.findAll({
-                order: [["createdAt", "DESC"]],
-                attributes: ["news_id", "title", "content", "fileName", "type"],
+                order: [['createdAt', 'DESC']],
+                attributes: ['news_id', 'title', 'content', 'fileName', 'type'],
             });
             allNews.map((e) => {
                 e.dataValues.imgUrl =
-                    "/files/assets/news/" + e.dataValues.fileName;
+                    '/files/assets/news/' + e.dataValues.fileName;
 
+                console.log(e.dataValues.date);
                 e.dataValues.date =
                     moment(e.dataValues.createdAt)
-                        .format("L h:mm")
+                        .format('L h:mm')
                         .slice(0, 11) +
                     (moment(e.dataValues.createdAt)
-                        .format("L h:mm")
+                        .format('L h:mm')
                         .slice(-5, -3) -
                         0 +
                         5) +
-                    moment(e.dataValues.createdAt).format("L h:mm").slice(-3);
+                    moment(e.dataValues.createdAt).format('L h:mm').slice(-3);
 
                 news.push(e.dataValues);
             });
